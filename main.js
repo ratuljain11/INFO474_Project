24cc8b8b98ff759062098d731c45773de3dc57bd
ratuljain11 <ratuljain11@gmail.com>
2021-12-05 14:34:41 -0800

    Completed bar plus interactivity

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-
-const svg = d3.select("svg"),
+function onCategoryChanged() {
+    var select = d3.select('#categorySelect').node();
+    // Get current value of select element
+    var category = select.options[select.selectedIndex].value;
+    // Update chart with the selected category of letters
+    updateChart(category);
+}
+updateChart('King');
+const svg = d3.select("#map"),
   width = +svg.attr("width"),
   height = +svg.attr("height");
 
@@ -7,7 +14,7 @@ const svg = d3.select("svg"),
 const path = d3.geoPath();
 const projection = d3.geoMercator()
   .scale(400)
-  .center([-100,20])
+  .center([-100,50])
   .translate([width / 2, height / 2]);
 
 // Data and color scale
@@ -19,7 +26,7 @@ const colorScale = d3.scaleThreshold()
 // Load external data and boot
 Promise.all([
 d3.json("states.geojson"),
-d3.csv("data.csv", function(d) {
+d3.csv("checker.csv", function(d) {
     data.set(d.code, +d.pop)
 })]).then(function(loadData){
 
@@ -85,10 +92,73 @@ d3.csv("data.csv", function(d) {
         .attr("text-anchor", "middle")
         .attr("alignment-baseline", "central")
         .style("font-size", 7)
-        .style("fill", "black")
-
-      
-   
+        .style("fill", "black")  
       
 })
+
+// set the dimensions and margins of the graph
+const margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width1 = 460 - margin.left - margin.right,
+    height1 = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg1 = d3.select("#bar")
+  .append("svg")
+    .attr("width", width1 + margin.left + margin.right)
+    .attr("height", height1 + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+
+function updateChart(countyName){
+    console.log(countyName);
+    // Parse the Data
+    d3.csv("SVI-Washington.csv").then( function(data) {
+    var temp = []; 
+    for(var i = 0; i < data.length; i++) {
+        if (data[i].county === countyName) {
+            temp.push(data[i]);
+        }
+    }
+    console.log(temp);
+
+    // X axis
+    const x = d3.scaleBand()
+      .range([ 0, width1 ])
+      .domain(temp.map(d => d.country))
+      .padding(0.2);
+    svg1.append("g")
+      .attr("transform", `translate(0, ${height1})`)
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+    
+    // Add Y axis
+    const y = d3.scaleLinear()
+      .domain([0, 1])
+      .range([ height1, 0]);
+    svg1.append("g")
+      .call(d3.axisLeft(y));
+
+    var u = svg1.selectAll("rect")
+        .data(temp)
+    
+      u
+        .enter()
+        .append("rect")
+        .merge(u)
+        .transition()
+        .duration(1000)
+          .attr("x", function(d) { return x(d.country); })
+          .attr("y", function(d) { return y(d.value); })
+          .attr("width", x.bandwidth())
+          .attr("height", function(d) { return height1 - y(d.value); })
+          .attr("fill", "#69b3a2")
+    
+    })
+    
+    
+}   
+
 
